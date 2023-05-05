c9070fe2b9bea0d256699dfa3504efd084185279
Amir <amirmiz33@gmail.com>
2023-05-05 12:07:17 +0300

    initial boot

---
+++ src/main/java/chargeit/chargesimulator/ChargeSimulatorApplication.java
@@ -0,0 +1,13 @@
+package chargeit.chargesimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargeSimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChargeSimulatorApplication.class, args);
+    }
+
+}
